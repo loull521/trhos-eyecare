1f263e504afcd9dc3092c60be4229243032116d1
jiaky777 <kunyangjia@gmail.com>
2013-12-12 17:28:21 +0800

    project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+trhos-eye
+====
+CCNT project for Tongren hospital
+
+本工程是浙江大学CCNT实验室为北京同仁医院开发的眼底图像相关服务项目。
---
+++ src/main/java/org/ccnt/msi/eyecare/App.java
@@ -0,0 +1,13 @@
+package org.ccnt.msi.eyecare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
